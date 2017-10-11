03678b679cf972664dfcead6d4dfd7b71e5a20a4
Tomas Pollak <tomaspollak@gmail.com>
2017-10-11 19:20:36 -0300

    Added simple build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set +e
+
+GCC=gcc
+
+if [ ! -d termbox ]; then
+  echo "Please clone the termbox repo here: git clone https://github.com/tomas/termbox"
+  exit 1
+fi
+
+# build termbox if we still haven't
+if [ ! -e termbox/build/libtermbox.a ]; then
+  cd termbox
+  rm -Rf build
+  mkdir build && cd build
+  cmake ..
+  make
+  cd ../..
+fi
+
+rm -f termbox.so lua-termbox.os
+$GCC $CFLAGS -I termbox/src/ -o lua-termbox.os -c -Wall -Werror -fPIC lua-termbox.c
+$GCC -o termbox.so -shared lua-termbox.os termbox/build/libtermbox.a

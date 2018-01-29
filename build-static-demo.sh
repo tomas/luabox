55a70725dca5010de20dff1c1443b9beaf22416e
Tomás Pollak <tomaspollak@gmail.com>
2018-01-29 20:18:20 -0300

    Added build-static-demo.sh

---
+++ build-static-demo.sh
@@ -0,0 +1,17 @@
+archives="lua-termbox.a termbox/build/libtermbox.a /usr/lib/x86_64-linux-gnu/libluajit-5.1.a"
+flags="-lm -no-pie -static"
+includes="-I /usr/include/luajit-2.0/"
+out="ui"
+
+echo " --> Building deps"
+sh build.sh
+echo " --> Building"
+# cc -Os -s demos/ui.lua.c $archives $flags $includes -o $out
+../luastatic/luastatic.lua demos/ui.lua $archives $includes $flags
+
+echo " --> Stripping"
+strip $out
+# strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id --remove-section=.note.ABI-tag $out
+
+ls -l $out | awk '{print $5}'
+echo "Outfile: ${out}"

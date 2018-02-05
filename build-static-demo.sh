469a9bd7bed625ef4d72ae4ce129fb83f4fa1323
Tomás Pollak <tomaspollak@gmail.com>
2018-02-05 11:00:58 -0300

    Progress

--- build-static-demo.sh
+++ build-static-demo.sh
@@ -1,14 +1,18 @@
 archives="luabox.a termbox/build/libtermbox.a /usr/lib/x86_64-linux-gnu/libluajit-5.1.a"
 flags="-lm -no-pie -static"
 includes="-I /usr/include/luajit-2.0/"
-out="ui"
+out="layout"
+deps="demos/lib/ui.lua demos/lib/classic.lua demos/lib/events.lua"
 
 echo " --> Building deps"
 sh build.sh
-echo " --> Building"
+
+rm -f "$out"
+echo " --> Building ${out}"
 # cc -Os -s demos/ui.lua.c $archives $flags $includes -o $out
-../luastatic/luastatic.lua demos/ui.lua $archives $includes $flags
-rm -f demos/ui.lua.c
+../luastatic/luastatic.lua demos/layout.lua $deps $archives $includes $flags
+
+# rm -f demos/layout.lua.c
 
 echo " --> Stripping"
 strip $out

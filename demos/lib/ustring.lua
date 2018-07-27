e926a5673a473d639c8ae6134ff0ebaa7c1d4675
Tomás Pollak <tomaspollak@gmail.com>
2018-07-27 04:02:58 -0400

    Use ustring.char when removing emoji

--- demos/lib/ustring.lua
+++ demos/lib/ustring.lua
@@ -329,7 +329,7 @@ function ustring.replaceEmoji(s, toChar)
   local toChar = toChar or ' '
   local chars = {}
   for cp in ustring.gcodepoint(s) do
-    table.insert(chars, isEmoji(cp) and toChar or string.char(cp))
+    table.insert(chars, isEmoji(cp) and toChar or ustring.char(cp))
   end
   return table.concat(chars, '')
 end

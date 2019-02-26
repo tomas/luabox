30a6ce5335007968112ddf878df0aafd2b360f18
Tomás Pollak <tomaspollak@gmail.com>
2019-02-25 23:05:56 -0300

    Replace emoji with empty circle

--- demos/lib/ui.lua
+++ demos/lib/ui.lua
@@ -865,7 +865,7 @@ function List:render_self()
     if not item then break end
 
     formatted = self:format_item(item)
-    if ustring.emojiCount(formatted) > 0 then formatted = ustring.replaceEmoji(formatted, '★') end
+    if ustring.emojiCount(formatted) > 0 then formatted = ustring.replaceEmoji(formatted, '◯') end
 
     diff = width - ustring.len(formatted)
 
